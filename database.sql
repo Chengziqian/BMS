0051044c8bba367120f442e9d5cd4d4919dd4b32
czq <18663104463@163.com>
2017-01-22 20:15:07 +0800

    pre-version 1.5

--- database.sql
+++ database.sql
@@ -5,18 +5,24 @@ CREATE TABLE BMS_users(
     `user_name` TEXT,
     `user_password` TEXT,
     `user_email` TEXT,
-    `user_type` TINYINT
+    `user_type` INT,
+    `user_lent_books` INT,
+    `user_allow_books` INT,
+    `user_waiting_books` INT
 );
 CREATE TABLE BMS_books(
-    `book_name` TEXT,
     `book_id` TEXT,
+    `book_name` TEXT,
+    `book_author` TEXT,
+    `book_type` TEXT,
     `book_pub` TEXT,
     `book_status` TEXT,
-    `book_lent_time` DATETIME,
+    `book_lent` INT,
+    `book_left` INT,
     `book_desc` LONGTEXT
 );
 CREATE TABLE BMS_user_info(
-    `mybook` TEXT,
+    `mybook_id` TEXT,
     `lenttime` DATETIME,
     `returntime` DATETIME,
     `bookstatus` TEXT
